42c91c2f9fdd2c2d6b23eb7aaac459466fa3d2fd
nyutopia <791206628@qq.com>
2017-04-24 10:58:35 +0800

    fanyi

--- weixinInterface.py
+++ weixinInterface.py
@@ -53,18 +53,18 @@ class WeixinInterface:
 	resp=urllib2.urlopen(url)
 	fanyi=json.loads(resp.read())
 	if fanyi['errorCode']==0:
-		if 'basic' in fanyi.keys():
-			trans=u"%s:\n%s\n%s\n网络释义:\n%s"%(fanyi['query']," ".join(fanyi['translation'])," ".join(fanyi['basic']['explains'])," ".join(fanyi['web'][0]['value']))
-		else:
-			trans=u'%s:\n基本翻译:%s\n'%(fanyi['query']," ".join(fanyi['translation']))
-		return trans
+	    if 'basic' in fanyi.keys():
+		trans=u"%s:\n%s\n%s\n网络释义:\n%s"%(fanyi['query']," ".join(fanyi['translation'])," ".join(fanyi['basic']['explains'])," ".join(fanyi['web'][0]['value']))
+	    else:
+		trans=u'%s:\n基本翻译:%s\n'%(fanyi['query']," ".join(fanyi['translation']))
+            return trans
 	elif fanyi['errorCode']==20:
-		return u'对不起，要翻译的文本过长'
+	    return u'对不起，要翻译的文本过长'
 	elif fanyi['errorCode']==30:
-		return u'对不起，无法进行有效的翻译'
+	    return u'对不起，无法进行有效的翻译'
 	elif fanyi['errorCode']==40:
-		return u'对不起，不支持的语言类型'
+	    return u'对不起，不支持的语言类型'
 	else:
-		return u'对不起，您输入的单词%s无法进行翻译，请检查拼写'% word
+	    return u'对不起，您输入的单词%s无法进行翻译，请检查拼写'% word
 
 	
