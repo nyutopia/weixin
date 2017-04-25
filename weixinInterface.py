a4a41c3703196e7881bbe2674efa4526522f8174
Nobody <nobody@yq150.onlinetools>
2017-04-25 16:34:36 +0800

    git commit by online edit file 1/weixinInterface.py

--- weixinInterface.py
+++ weixinInterface.py
@@ -8,6 +8,28 @@ import urllib2
 import json
 from lxml import etree
 
+def youdao(word):
+        qword=urllib2.quote(word)
+        #baseurl=r"https://fanyi.youdao.com/openapi.do?keyfrom=nyutopia&key=1909528419&type=data&doctype=json&version=1.1&q="
+        baseurl=r"https://fanyi.youdao.com/openapi.do?keyfrom=nyannew&key=1896715416&type=data&doctype=json&version=1.1&q="
+        url=baseurl+qword
+        resp=urllib2.urlopen(url)
+        fanyi=json.loads(resp.read())
+        if fanyi['errorCode']==0:
+            if 'basic' in fanyi.keys():
+                trans=u"%s:\n%s\n%s\n网络释义:\n%s"%(fanyi['query']," ".join(fanyi['translation'])," ".join(fanyi['basic']['explains'])," ".join(fanyi['web'][0]['value']))
+            else:
+                trans=u'%s:\n基本翻译:%s\n'%(fanyi['query']," ".join(fanyi['translation']))
+            return trans
+        elif fanyi['errorCode']==20:
+            return u'对不起，要翻译的文本过长'
+        elif fanyi['errorCode']==30:
+            return u'对不起，无法进行有效的翻译'
+        elif fanyi['errorCode']==40:
+            return u'对不起，不支持的语言类型'
+        else:
+            return u'对不起，您输入的单词%s无法进行翻译，请检查拼写'% word
+        
 class WeixinInterface:
     def __init__(self):
     	self.app_root=os.path.dirname(__file__)
@@ -35,27 +57,7 @@ class WeixinInterface:
         if hashcode == signature:
             return echostr
    
-    def youdao(word):
-        qword=urllib2.quote(word)
-        #baseurl=r"https://fanyi.youdao.com/openapi.do?keyfrom=nyutopia&key=1909528419&type=data&doctype=json&version=1.1&q="
-        baseurl=r"https://fanyi.youdao.com/openapi.do?keyfrom=nyannew&key=1896715416&type=data&doctype=json&version=1.1&q="
-        url=baseurl+qword
-        resp=urllib2.urlopen(url)
-        fanyi=json.loads(resp.read())
-        if fanyi['errorCode']==0:
-            if 'basic' in fanyi.keys():
-                trans=u"%s:\n%s\n%s\n网络释义:\n%s"%(fanyi['query']," ".join(fanyi['translation'])," ".join(fanyi['basic']['explains'])," ".join(fanyi['web'][0]['value']))
-            else:
-                trans=u'%s:\n基本翻译:%s\n'%(fanyi['query']," ".join(fanyi['translation']))
-            return trans
-        elif fanyi['errorCode']==20:
-            return u'对不起，要翻译的文本过长'
-        elif fanyi['errorCode']==30:
-            return u'对不起，无法进行有效的翻译'
-        elif fanyi['errorCode']==40:
-            return u'对不起，不支持的语言类型'
-        else:
-            return u'对不起，您输入的单词%s无法进行翻译，请检查拼写'% word
+    
     def POST(self):
         str_xml=web.data() #获得post来的数据
         xml = etree.fromstring(str_xml)#进行xml解析
