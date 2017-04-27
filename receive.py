d170b9fd39f64480b303bac80827aaf806aac348
Nobody <nobody@yq150.onlinetools>
2017-04-27 16:03:27 +0800

    git commit by online edit file 1/receive.py

--- receive.py
+++ receive.py
@@ -13,6 +13,8 @@ def parse_xml(web_data):
         return TextMsg(xmlData)
     elif msg_type == 'image':
         return ImageMsg(xmlData)
+    elif mag_type = 'event':
+        return EventMsg(xmlData)
 
 class Msg(object):
     def __init__(self,xmlData):
@@ -20,16 +22,23 @@ class Msg(object):
         self.FromUserName=xmlData.find("FromUserName").text
         self.CreateTime=xmlData.find("CreateTime").text
         self.MsgType=xmlData.find("MsgType").text
-        self.MsgId=xmlData.find("MsgId").text
+        
 
 class TextMsg(Msg):
     def __init__(self,xmlData):
         Msg.__init__(self,xmlData)
+        self.MsgId=xmlData.find("MsgId").text
         self.Content=xmlData.find("Content").text.encode("utf-8")
 
 class ImageMsg(Msg):
     def __init__(self,xmlData):
         Msg.__init__(self,xmlData)
+        self.MsgId=xmlData.find("MsgId").text
         self.PicUrl=xmlData.find("PicUrl").text
         self.MediaId=xmlData.find("MediaId").text
 
+class EventMsg(Msg):
+    def __init__(self,xmlData):
+        Msg.__init__(self,xmlData)
+        self.Event=xmlData.find("Event").text
+        
