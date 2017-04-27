46ef7cf18f9e4fd0b1679e517e0cc9a80b5c8f50
Nobody <nobody@yq150.onlinetools>
2017-04-27 16:09:39 +0800

    git commit by online edit file 1/weixinInterface.py

--- weixinInterface.py
+++ weixinInterface.py
@@ -98,7 +98,7 @@ class WeixinInterface:
                     mediaId = recMsg.MediaId
                     return self.render.reply_image(fromUser,toUser,int(time.time()),mediaId)
                 elif recMsg.MsgType == 'event':
-                    if recMsg.Event = "subscribe":
+                    if recMsg.Event == "subscribe":
                         content = u"欢迎关注本订阅号，这个订阅号是本人业余爱好所建立"
                         return self.render.reply_text(fromUser,toUser,int(time.time()),content)
                 else:
