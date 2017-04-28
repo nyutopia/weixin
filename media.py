8ca9a0976fbd8bd671c6377b7d357d1d84da4dd3
Nobody <nobody@yq150.onlinetools>
2017-04-28 15:29:04 +0800

    git commit by online edit file 1/media.py

--- media.py
+++ media.py
@@ -16,12 +16,11 @@ class Media(object):
         postData,postHeaders=poster.encode.multipart_encode(param)
  
         postUrl = "https://api.weixin.qq.com/cgi-bin/media/upload?access_token=%s&type=%s"%(accessToken,mediaType)
-        try:
-            request=urllib2.Request(postUrl,postData,postHeaders)
-            urlResp = urllib2.urlopen(request)
-            print urlResp.read()
-        except Exception,e:
-            print "exception " 
+        
+        request=urllib2.Request(postUrl,postData,postHeaders)
+        urlResp = urllib2.urlopen(request)
+        print urlResp.read()
+        
     def GET(self):
      	accessToken = Basic().get_access_token()
         root=os.path.dirname(__file__)
