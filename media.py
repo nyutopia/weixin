dfd54010aa3f3ff7e27df0c2c881be7653bfbf61
Nobody <nobody@yq150.onlinetools>
2017-04-28 15:52:14 +0800

    git commit by online edit file 1/media.py

--- media.py
+++ media.py
@@ -5,21 +5,19 @@ import urllib2
 import os
 import poster.encode
 from poster.streaminghttp import register_openers
-def upload(accessToken,filePath,mediaType):
-    openFile=open(filePath,"rb")
-    param={"media":openFile}
-    postData,postHeaders=poster.encode.multipart_encode(param)
- 
-    postUrl = "https://api.weixin.qq.com/cgi-bin/media/upload?access_token=%s&type=%s"%(accessToken,mediaType)
-        
-    request=urllib2.Request(postUrl,postData,postHeaders)
-    urlResp = urllib2.urlopen(request)
-    print urlResp.read()
+
 class Media(object):
     def __init__(self):
         register_openers()
     #上传图片
-    
+    def upload(accessToken,filePath,mediaType):
+        openFile=open(filePath,"rb")
+        param={"media":openFile}
+        postData,postHeaders=poster.encode.multipart_encode(param)
+        postUrl = "https://api.weixin.qq.com/cgi-bin/media/upload?access_token=%s&type=%s"%(accessToken,mediaType)
+        request=urllib2.Request(postUrl,postData,postHeaders)
+        urlResp = urllib2.urlopen(request)
+        print urlResp.read()
         
     def GET(self):
      	accessToken = Basic().get_access_token()
